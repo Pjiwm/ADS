d5abab4685408333f9bb2cd202a8e6392e1c8db1
Pjiwm <Pim.Munne@gmail.com>
2021-10-05 23:34:15 +0200

    changed NNode with father and mother attributes

--- src/familytree/NNode.java
+++ src/familytree/NNode.java
@@ -1,17 +1,34 @@
 package familytree;
 
+import java.util.LinkedList;
 import java.util.List;
 
 public class NNode<T> {
-    private T value;
-    private T left, right;
+    private T father;
+    private T mother;
+    private List<NNode<T>> children;
 
-    public NNode(T value) {
-        this.value = value;
+    public NNode(T father, T mother) {
+        this.father = father;
+        this.mother = mother;
+        this.children = new LinkedList<>();
     }
 
-    public void insert(T left, T right) {
-        this.left = left;
-        this.right = right;
+    public void insert(LinkedList<NNode<T>> children) {
+        this.children = children;
     }
+
+
+    public T getFather() {
+        return this.father;
+    }
+
+    public T getMother() {
+        return this.mother;
+    }
+
+    public List<NNode<T>> getChildren() {
+        return this.children;
+    }
+
 }
